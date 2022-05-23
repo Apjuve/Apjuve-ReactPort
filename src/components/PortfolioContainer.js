64e97d3123abbbbd98ab95291606c66f69baa350
Apjuve <abiponce3@hotmail.com>
2022-05-22 20:38:50 -0400

    created render page function for portfolio contain

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Navbar from "./navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState("About");
+
+    const renderPage = () => {
+        if (currentPage === "Projects") {
+            return <Projects />;
+        }
+        if (currentPage === "About") {
+            return <About />;
+        }
+        if (currentPage === "Resume") {
+            return <Resume />;
+        }
+        return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+    return (
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+
+            {renderPage()}
+        </div>
+    )
+}
