045bbe7a7dfecafe4f8ab91b2b13c089535e5cf6
Apjuve <abiponce3@hotmail.com>
2022-07-30 13:31:26 -0400

    updated resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,34 +4,10 @@ import "../styles/Resume.css";
 
 export default function Resume() {
     return (
-      <div>
-        <h1 className="resume-header">Resume</h1>
-  
-        <p className="coding-icons">Languages and Technologies:</p>
-        <p className="icons">
-          <i className="fab fa-html5"> </i>
-          <i className="fab fa-css3-alt"> </i>
-          <i className="fab fa-js-square"> </i>
-          <i className="fab fa-react"> </i>
-          <i class="fab fa-github"></i>
-          <i class="fab fa-npm"></i>
-          <i class="fab fa-markdown"></i>
-          <i class="fab fa-bootstrap"></i>
-          <i class="fab fa-accessible-icon"></i>
-        </p>
-        <p className="download">
-          Click{" "}
-          {/* <a
-            className="link"
-            href={PonceResume}
-            download="resume"
-            target="_blank"
-            rel="noreferrer"
-          >
-            here
-          </a>{" "} */}
-          to download a copy of my resume!
-        </p>
-      </div>
+      <section className="resume">
+        <div className="is-size-4 p-3">
+                <a href="" download="Aponce">Download Resume</a>
+            </div>
+      </section>
     );
   }
